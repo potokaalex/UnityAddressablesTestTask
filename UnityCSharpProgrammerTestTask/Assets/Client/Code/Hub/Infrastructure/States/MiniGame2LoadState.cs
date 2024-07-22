12a1ab080ade8a4bef9c43c767eb55256d4fb40a
Alenya <nevskial686@gmail.com>
2024-07-22 20:30:05 +0300

    Mini games loading screen

--- UnityCSharpProgrammerTestTask/Assets/Client/Code/Hub/Infrastructure/States/MiniGame2LoadState.cs
+++ UnityCSharpProgrammerTestTask/Assets/Client/Code/Hub/Infrastructure/States/MiniGame2LoadState.cs
@@ -1,6 +1,7 @@
 ﻿using Client.Common.Services.ConfigProvider;
 using Client.Common.Services.SceneLoader;
 using Client.Common.Services.StateMachine;
+using Client.Common.UI.Factories.Global;
 using Cysharp.Threading.Tasks;
 
 namespace Client.Hub.Infrastructure.States
@@ -9,17 +10,23 @@ namespace Client.Hub.Infrastructure.States
     {
         private readonly IConfigProvider _configProvider;
         private readonly ISceneLoader _sceneLoader;
+        private readonly IGlobalUIFactory _uiFactory;
 
-        public MiniGame2LoadState(IConfigProvider configProvider, ISceneLoader sceneLoader)
+        public MiniGame2LoadState(IConfigProvider configProvider, ISceneLoader sceneLoader, IGlobalUIFactory uiFactory)
         {
             _configProvider = configProvider;
             _sceneLoader = sceneLoader;
+            _uiFactory = uiFactory;
         }
 
         public async UniTask Enter()
         {
+            var loadingWindow = _uiFactory.CreateLoadingWindow();
+            loadingWindow.Open();
+            
             var key = _configProvider.Configs.Scenes.MiniGame2Key;
-            await _sceneLoader.LoadSceneAsync(key);
+            await _sceneLoader.LoadSceneAsync(key, f => loadingWindow.SetProgress(f, 0, 1));
+            _uiFactory.Destroy(loadingWindow);
         }
 
         public UniTask Exit() => UniTask.CompletedTask;
